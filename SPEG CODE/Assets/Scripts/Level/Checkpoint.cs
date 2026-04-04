84ac7964e4f02d6b1b6e1489e37b41f393f61d6b
HasNate618 <nate.e.espejo@gmail.com>
2026-04-04 17:17:24 -0400

    implement scene checkpoints

---
+++ SPEG CODE/Assets/Scripts/Level/Checkpoint.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    static bool hasCheckpoint;
+    static Vector3 position;
+    static float rotation;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        position = transform.position;
+        rotation = transform.eulerAngles.y;
+        hasCheckpoint = true;
+    }
+
+    public static void Restore(Transform player)
+    {
+        wif (!hasCheckpoint) return;
+        var cc = player.GetComponent<CharacterController>();
+        if (cc != null) cc.enabled = false;
+        player.SetPositionAndRotation(position, Quaternion.Euler(0f, rotation, 0f));
+        if (cc != null) cc.enabled = true;
+    }
+}
